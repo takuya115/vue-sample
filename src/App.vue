178312c3a6ca52c85212243f8d49ee8b6d2169d1
TakuyaUrushibara <ut1995j15@gmail.com>
2021-01-09 10:29:47 +0900

    SVGコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <SVGRect :posX="X" :posY="Y" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import SVGRect from './components/SVGRect.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SVGRect,
+  },
+  data() {
+    return {
+      X: 0,
+      Y: 0,
+    }
   }
 }
 </script>
@@ -21,8 +29,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/SVGRect.vue
@@ -0,0 +1,29 @@
+<template>
+<div style="width:100%; height:100%;">
+    <svg width="60" height="60" 
+        viewPort="0 0 120 120" version="1.1"
+        xmlns="http://www.w3.org/2000/svg">
+    
+        <rect :x="posX+5" :y="posY+5" 
+            width="50" height="50"
+            rx="15" ry="15"/>
+    </svg>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'SVGRect',
+    props: ['posX', 'posY'],
+    computed: {
+        getTranslate() {
+            return `translate(${this.posX}, ${this.posY})`
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
